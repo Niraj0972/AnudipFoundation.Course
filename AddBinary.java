96a3486bba0e3085f3ab3e87f382591cbeaf2f9d
Niraj0972 <birariniraj@gmail.com>
2025-06-13 00:21:43 +0530

    Add files via upload

---
+++ AddBinary.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first binary number: ");
+        String a = sc.nextLine();
+        System.out.print("Enter second binary number: ");
+        String b = sc.nextLine();
+        int sum = Integer.parseInt(a,2) + Integer.parseInt(b, 2);
+        System.out.println("Sum = " + Integer.toBinaryString(sum));
+        sc.close();
+    }
+}
